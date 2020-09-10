1605a959cc720d4a8de3fac5982d3145773f98e1
zizynonno <zizynonno@gmail.com>
2020-09-11 02:10:32 +0900

    twitter callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
   root 'pages#index'
   get 'pages/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
