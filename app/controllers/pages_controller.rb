775e2f1097af9d0d21043a392693f44323bd97f2
zizynonno <zizynonno@gmail.com>
2020-09-29 00:43:09 +0900

    add: bootstrapcard testdata

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
   def index
+    # @users = User.all
+    # users = User.all
+    # users.each do |user|
+    #   @contents = user.contents.paginate(page: params[:page])
+    # end
+    @contents = Content.all
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "securerandom"
+
+User.create!(username: 'hadson', email: 'admin@phathoth.com', password: 'password', confirmed_at: Time.now)
+
+99.times do |n|
+  username = SecureRandom.urlsafe_base64(10)
+  email = "example-#{n+1}@plathoth.com"
+  password = "password"
+  User.create!(username: username,
+               email: email,
+               password:              password,
+               confirmed_at: Time.now)
+end
+
+users = User.order(:created_at).take(6)
+50.times do |n|
+  title = "phathothのイメージ#{n+1}"
+  summary = Faker::Lorem.sentence(5)
+  view = rand(10000)
+  media = open("#{Rails.root}/db/fixtures/00.jpg")
+  users.each { |user| user.contents.create!(title: title,
+                                            summary: summary,
+                                            view: view,
+                                            media: media) }
+end
