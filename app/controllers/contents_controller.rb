e461e9e7a3931e556055a43071c6bf0768facbe4
zizynonno <zizynonno@gmail.com>
2020-10-05 00:46:00 +0900

    add: content new create

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -3,4 +3,28 @@ class ContentsController < ApplicationController
     @content = Content.find(params[:id])
     @extension_type = File.extname("#{@content.media}")
   end
+
+  def new
+    if user_signed_in?
+      @content = current_user.contents.new 
+    else
+      redirect_to root_path
+    end
+  end
+
+  def create
+    @content = current_user.contents.build(content_params)
+    if @content.save
+      flash[:success] = "Content created!"
+      redirect_to root_url
+    else
+      render new_content
+    end
+  end
+
+  private
+
+    def content_params
+      params.require(:content).permit(:title,:summary,:media)
+    end
 end
